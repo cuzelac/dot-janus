40981a81e4abe88fb88fe5f56c0670f2f3f0f0ac
Chris Uzelac <cu@squareup.com>
2013-10-28 13:33:41 -0700

    add vividchalk fork

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ mkdir ~/.janus
 cd ~/.janus
 git clone https://github.com/jnwhiteh/vim-golang.git go
 git clone https://github.com/cuzelac/AutoTag.git
+git clone https://github.com/cuzelac/vim-vividchalk.git vividchalk
+git clone https://github.com/rizzatti/funcoo.vim.git
+git clone https://github.com/rizzatti/dash.vim.git
