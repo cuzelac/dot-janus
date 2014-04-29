48cc9117f7ddf38dd4c667b4a0b963b7385ef019
Chris Uzelac <cu@squareup.com>
2014-04-29 12:13:28 -0700

    proto colors

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ git clone https://github.com/cuzelac/AutoTag.git
 git clone https://github.com/cuzelac/vim-vividchalk.git vividchalk
 git clone https://github.com/rizzatti/funcoo.vim.git
 git clone https://github.com/rizzatti/dash.vim.git
+git clone https://github.com/garyharan/vim-proto.git
