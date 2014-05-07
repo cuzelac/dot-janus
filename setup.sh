8011d56a64dd7451355b122424eaaabcaa0bc6dd
Chris Uzelac <cu@squareup.com>
2014-05-07 10:09:23 -0700

    add taboo

--- setup.sh
+++ setup.sh
@@ -8,3 +8,4 @@ git clone https://github.com/cuzelac/vim-vividchalk.git vividchalk
 git clone https://github.com/rizzatti/funcoo.vim.git
 git clone https://github.com/rizzatti/dash.vim.git
 git clone https://github.com/garyharan/vim-proto.git
+git clone https://github.com/gcmt/taboo.vim.git
