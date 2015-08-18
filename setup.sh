dc87551203e130e4bf12cd5ad981a1985dd6aa24
Chris Uzelac <cu@squareup.com>
2015-08-18 10:51:16 -0700

    add coffeescript stuff

--- setup.sh
+++ setup.sh
@@ -9,5 +9,7 @@ git clone https://github.com/rizzatti/funcoo.vim.git
 git clone https://github.com/rizzatti/dash.vim.git
 git clone https://github.com/garyharan/vim-proto.git
 git clone https://github.com/gcmt/taboo.vim.git
+git clone https://github.com/lukaszkorecki/CoffeeTags.git
+git clone https://github.com/kchmck/vim-coffee-script.git
 
 gem install CoffeeTags --no-rdoc --no-ri
