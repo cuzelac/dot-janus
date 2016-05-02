74e8d889a670b5295d0b842a53d7f5d49db88ec5
Chris Uzelac <cu@squareup.com>
2016-05-02 13:04:02 -0700

    improvements

--- setup.sh
+++ setup.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
-mkdir ~/.janus
-cd ~/.janus
-git clone https://github.com/fatih/vim-go.git
-git clone https://github.com/cuzelac/AutoTag.git
-git clone https://github.com/cuzelac/vim-vividchalk.git vividchalk
-git clone https://github.com/rizzatti/funcoo.vim.git
-git clone https://github.com/rizzatti/dash.vim.git
-git clone https://github.com/garyharan/vim-proto.git
-git clone https://github.com/gcmt/taboo.vim.git
-git clone https://github.com/lukaszkorecki/CoffeeTags.git
-git clone https://github.com/kchmck/vim-coffee-script.git
-git clone https://github.com/bling/vim-airline.git
+set +x
 
-gem install CoffeeTags --no-rdoc --no-ri
+# curl -SsL https://raw.githubusercontent.com/cuzelac/dot-janus/master/setup.sh | bash
+
+function bg_run() {
+  cmd=$1
+
+  if [[ 1 == $DEBUG ]]
+  then
+    bash -c "${cmd}"
+  else
+    bash -c "${cmd} > /dev/null 2>&1 &"
+  fi
+}
+
+INSTALL_DIR=~/.janus
+
+git clone git@github.com:cuzelac/dot-janus.git $INSTALL_DIR
+
+pushd $INSTALL_DIR
+
+bg_run "git clone https://github.com/fatih/vim-go.git"
+bg_run "git clone https://github.com/cuzelac/AutoTag.git"
+bg_run "git clone https://github.com/tpope/vim-vividchalk.git"
+bg_run "git clone https://github.com/rizzatti/funcoo.vim.git"
+bg_run "git clone https://github.com/rizzatti/dash.vim.git"
+bg_run "git clone https://github.com/garyharan/vim-proto.git"
+bg_run "git clone https://github.com/gcmt/taboo.vim.git"
+bg_run "git clone https://github.com/lukaszkorecki/CoffeeTags.git"
+bg_run "git clone https://github.com/kchmck/vim-coffee-script.git"
+bg_run "git clone https://github.com/bling/vim-airline.git"
+
+bg_run "gem install CoffeeTags --no-rdoc --no-ri"
+
+popd $INSTALL_DIR
+
+wait
