28e7b8cdb1a62428a8674ef312e1915c237f71f7
Chris Uzelac <cu@squareup.com>
2017-10-05 16:52:23 -0700

    use vim-ruby

--- setup.sh
+++ setup.sh
@@ -32,6 +32,7 @@ bg_run "git clone https://github.com/gcmt/taboo.vim.git"
 bg_run "git clone https://github.com/lukaszkorecki/CoffeeTags.git"
 bg_run "git clone https://github.com/kchmck/vim-coffee-script.git"
 bg_run "git clone https://github.com/bling/vim-airline.git"
+bg_run "git clone https://github.com/vim-ruby/vim-ruby.git"
 
 bg_run "gem install CoffeeTags --no-rdoc --no-ri"
 
